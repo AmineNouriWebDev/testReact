39d9f69d8df5c63e5d75e27dab374f38a74d9038
AmineNouriWebDev <nouri.webdev@gmail.com>
2025-04-06 18:21:32 +0100

    nettoyage main

--- reactCourse/src/App.jsx
+++ reactCourse/src/App.jsx
@@ -1,26 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import MyButton from "./components/MyButton";
-const person = {
-  name: "amine",
-  age: 25,
-  job: "developer",
-};
+
 function App() {
-  const elmStyle = {
-    color: "red",
-    backgroundColor: "yellow",
-    fontSize: "30px",
-    textAlign: "center",
-    padding: "20px 0px",
-  };
-  return (
-    <>
-      <h1 style={elmStyle}>
-        Hello {person.name} you are {person.job} your age is: {person.age}
-      </h1>
-      <MyButton />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
