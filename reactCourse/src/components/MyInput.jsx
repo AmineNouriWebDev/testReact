69d8b49e893fedef5c69794282ae18d35130268a
AmineNouriWebDev <nouri.webdev@gmail.com>
2025-04-11 22:23:05 +0100

    use state

--- reactCourse/src/components/MyInput.jsx
+++ reactCourse/src/components/MyInput.jsx
@@ -8,6 +8,10 @@ const MyInput = () => {
     name: "",
     email: "",
     age: "",
+    generalInfo: "",
+    isMajor: false,
+    country: "",
+    status: "",
   });
 
   return (
@@ -47,6 +51,67 @@ const MyInput = () => {
             setInputsValue({ ...inputsValue, age: e.target.value })
           }
           value={inputsValue.age}
+        />
+        <br />
+        <hr></hr>
+        <label>Country</label> <br></br>
+        <select
+          value={inputsValue.country}
+          name="country"
+          onChange={(e) =>
+            setInputsValue({ ...inputsValue, country: e.target.value })
+          }>
+          <option value="">Select a country</option>
+          <option>Tunisia</option>
+          <option>Egypt</option>
+          <option>Japon</option>
+        </select>
+        <hr></hr>
+        <div>
+          <input
+            value="teacher"
+            type="radio"
+            name="teacher"
+            id="teacher"
+            checked={inputsValue.status == "teacher"}
+            onChange={(e) =>
+              setInputsValue({ ...inputsValue, status: e.target.value })
+            }
+          />
+          Teacher
+          <input
+            value="student"
+            type="radio"
+            name="student"
+            id="student"
+            checked={inputsValue.status == "student"}
+            onChange={(e) =>
+              setInputsValue({ ...inputsValue, status: e.target.value })
+            }
+          />
+          Student
+        </div>
+        <hr></hr>
+        <br />
+        <label htmlFor="generalInfo"></label>
+        <textarea
+          value={inputsValue.generalInfo}
+          name="generalInfo"
+          id="generalInfo"
+          onChange={(e) =>
+            setInputsValue({ ...inputsValue, generalInfo: e.target.value })
+          }
+        />
+        <br />
+        <label htmlFor="major">is Major?</label>
+        <input
+          type="checkbox"
+          name="major"
+          id="major"
+          onChange={(e) =>
+            setInputsValue({ ...inputsValue, isMajor: e.target.checked })
+          }
+          checked={inputsValue.isMajor}
         />
         <hr />
         <button type="submit">send</button>
